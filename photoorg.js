03dc844792f5f908f3d161257868b73a69abb6df
Simon <simon.nicholas@outlook.com>
2025-06-20 22:42:51 +1000

    Added some changes to filter out faulty date info and program gives options to move unknown files from date modified

--- photoorg.js
+++ photoorg.js
@@ -2,6 +2,13 @@ const exifr = require('exifr'); // For obtaining photo metadata
 const ffmpeg = require('fluent-ffmpeg');
 const fs = require('fs/promises');
 const path = require('path');
+const readline = require('readline');
+
+// Input/output handler
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 // Gets data taken from video metadata
 async function getVideoTakenDate(filePath) {
@@ -18,19 +25,23 @@ async function getVideoTakenDate(filePath) {
   if unable to do so (most likely a video)
 */
 async function getTakenDate(filePath) {
-  // This will work if an image
+  // This should work if an image
   try {
     const { DateTimeOriginal } = await exifr.parse(filePath);
-    if (DateTimeOriginal) return DateTimeOriginal;
+    if (DateTimeOriginal instanceof Date && !isNaN(DateTimeOriginal)) {
+       return DateTimeOriginal;
+    }
   } catch (err) {
     console.error('err');
     console.log(`${filePath} is probably a video`)
   }
 
-  // This will work for videos
+  // This dhould work for videos
   try {
     const videoDate = await getVideoTakenDate(filePath);
-    if (videoDate) return videoDate;
+    if (videoDate instanceof Date && !isNaN(videoDate)) {
+      return parsed;
+    }
   } catch (err) {
     console.error(err);
   }
@@ -39,23 +50,62 @@ async function getTakenDate(filePath) {
   return null;
 }
 
-(async () => {
-  const baseDir = path.resolve(__dirname, './../');
-  const fallBackDir = path.join(baseDir, 'unknown dates');
+// Gets date modified - for moving files with unknown taken dates
+async function getModifiedDate(filePath) {
+  try {
+    const stats = await fs.stat(filePath);
+    return stats.mtime;
+  } catch (err) {
+    console.error(err);
+    return null
+  }
+}
 
-  // Obtain all files (filtering photoorg.js) are log them
-  const allFiles = await fs.readdir(baseDir, { withFileTypes: true });
+// Obtain all files, log them and return them
+async function getFiles(dir) {
+  const allFiles = await fs.readdir(dir, { withFileTypes: true });
   const files = allFiles
     .filter(file => file.isFile())
     .map(file => file.name);
 
   console.log(`Number of files to be organised: ${files.length}
-    \nThe files are as follows:\n`);
+    \nThe files are as follows:\n${files}`);
+
+  return files;
+}
+
+// FOr moving files into date folders
+async function moveFile(dir, date, file, filePath) {
+  try{
+    // Obtain year and month from Date object and put in according folder
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const monthDir = path.join(dir, String(year), `${year}-${month}`);
+    await fs.mkdir(monthDir, { recursive: true});
+    const destPath = path.join(monthDir, file)
+    await fs.rename(filePath, destPath);
+    console.log(`${file} moved to ${monthDir}`);
+    console.log(''); // Blank line for next file
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+/* The main organiser function - calls above functions to get dates organise
+  photos into a year/year-month folder system. Files are moved to "unknown
+  dates" folder if dates are not obtained from meta data
+*/
+async function organisePhotos() {
+  const baseDir = path.resolve(__dirname, './../');
+  const fallBackDir = path.join(baseDir, 'unknown dates');
+
+  const files = await getFiles(baseDir);
   
   for(const file of files) {
     try {
       const filePath = path.join(baseDir, file);
-      const date = await getTakenDate(filePath)
+      const date = await getTakenDate(filePath);
+      console.log(date);
 
       // If no date, put them in "unknown dates" folder
       if (!date) {
@@ -66,17 +116,62 @@ async function getTakenDate(filePath) {
         continue;
       }
       
-      // Obtain year and month from Date object and put in according folder
-      const year = date.getFullYear();
-      const month = String(date.getMonth() + 1).padStart(2, '0');
-      const monthDir = path.join(baseDir, String(year), `${year}-${month}`);
-      await fs.mkdir(monthDir, { recursive: true});
-      const destPath = path.join(monthDir, file)
-      await fs.rename(filePath, destPath);
-      console.log(`${file} moved to ${monthDir}`);
-      console.log(''); // Blank line for next file
+      await moveFile(baseDir, date, file, filePath);
     } catch (err) {
       console.error(`CAN'T PROCESS ${file}: ${err}`)
     }
   }
-})();
+};
+
+// Allows files from "unknown dates" folder to be moved based on date modified
+async function organiseUnknown() {
+  const baseDir = path.resolve(__dirname, './../');
+  const unknownDir = path.resolve(baseDir, 'unknown dates');
+
+  const files = await getFiles(unknownDir);
+
+  for (file of files) {
+    try {
+      const filePath = path.join(unknownDir, file);
+      const date = await getModifiedDate(filePath);
+
+      // If no date, leave them in "unknown dates" folder
+      if (!date) {
+        console.log(`NO DATE FOR ${file}`);
+        continue;
+      }
+
+      await moveFile(baseDir, date, file, filePath);
+    } catch (err) {
+      console.error(`CAN'T PROCESS ${file}: ${err}`)
+    }
+  }
+}
+
+(async () => {
+  rl.question(`Pick an Option
+    1. Move all files in base folder into dated folders based on metadata (date taken)
+    2. Move files in "unknown dates" folder based on their sate modifed
+    3. Exit
+    `, (selection) => {
+      switch(selection.trim()) {
+        case '1':
+          organisePhotos();
+          rl.close();
+          break;
+        case '2':
+          organiseUnknown();
+          rl.close();
+          break;
+        case '3':
+          running = false;
+          rl.close();
+          process.exit();
+        default:
+          console.log('Invalid selection');
+          rl.close();
+      }
+  });
+})()
+
+
